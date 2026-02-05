1083309a7a235bb30320faf34400f565828bd1b4
Rituraj Bhattacharjee <86673376+dePolanball@users.noreply.github.com>
2026-02-05 20:10:30 +0530

    Change API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "strong-ceil-homeproject-d2a6849c.koyeb.app/",
 });
 
 api.interceptors.request.use((config) => {
