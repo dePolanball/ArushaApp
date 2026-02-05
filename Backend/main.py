5e5cff07bdebf47b300534d4922410da830b1d30
Rituraj Bhattacharjee <86673376+dePolanball@users.noreply.github.com>
2026-02-05 12:50:13 +0530

    Add allowed origin for deployed frontend

--- Backend/main.py
+++ Backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://dePolanball.github.io"
         # add your deployed frontend here later
     ],
     allow_credentials=True,
