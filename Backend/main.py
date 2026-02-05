009e95bd4e632806cdc76e5ce08e0f2faa228da8
Rituraj Bhattacharjee <86673376+dePolanball@users.noreply.github.com>
2026-02-05 10:28:35 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi import FastAPI
-from sheets import fetch_sheet_rows
 from analytics import get_cached
 
 # -----------------------------
